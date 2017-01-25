688e78d24e7b8a2040ea4dcf2ca7fb956f104f33
Ben Mulligan <benpmulligan@gmail.com>
2017-01-25 00:40:12 -0500

    Add wall behaviour

---
+++ Rogue-Like/Assets/Scripts/Wall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+	public Sprite dmgSprite;
+	public int hp = 4;
+
+	private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Awake () {
+		this.spriteRenderer = this.GetComponent<SpriteRenderer> ();
+	}
+
+	public void DamageWall(int dmg) {
+
+		if (dmg > 0) {
+			spriteRenderer.sprite = dmgSprite;
+		}
+
+		hp -= dmg;
+		this.gameObject.SetActive (this.hp > 0);
+	}
+}
