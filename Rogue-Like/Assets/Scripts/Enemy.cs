9183f1c4a4fee6bbc4bfa96bdccccb1176ca2552
Ben Mulligan <benpmulligan@gmail.com>
2017-01-25 22:51:09 -0500

    enemy script

---
+++ Rogue-Like/Assets/Scripts/Enemy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MovingObject {
+
+	public int playerDamage;
+
+	private Animator animator;
+	private Transform target;
+	private bool skipMove;
+
+	// Use this for initialization
+	protected override void Start () {
+		this.animator = GetComponent<Animator> ();
+		this.target = GameObject.FindGameObjectWithTag ("Player").transform;
+		base.Start ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	protected override void AttemptMove<T> (int xDir, int yDir)
+	{
+		if (this.skipMove) {
+			this.skipMove = false;
+			return;
+		}
+
+		base.AttemptMove<T> (xDir, yDir);
+
+		this.skipMove = true;
+	}
+
+	protected void MoveEnemy() {
+		
+		int xDir = 0;
+		int yDir = 0;
+
+		if (Mathf.Abs (this.target.position.x - this.transform.position.x) <= float.Epsilon) {
+			yDir = this.target.position.y > this.transform.position.y ? 1 : -1;
+		} else {
+			xDir = this.target.position.x > this.transform.position.x ? 1 : -1;
+		}
+
+		this.AttemptMove<Player> (xDir, yDir);
+	}
+
+	protected override void OnCantMove<T> (T component)
+	{
+		Player hitPlayer = component as Player;
+		hitPlayer.LoseFood (this.playerDamage);
+	}
+}
